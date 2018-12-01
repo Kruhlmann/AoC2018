d9938aa09933528f8efa495347218968001917a7
Andreas Kruhlmann <andreas@kruhlmann.com>
2018-12-01 11:35:05 +0100

    1st day complete

---
+++ 1/advent.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const frequency_dataset = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+let dataset = fs.readFileSync("input.txt", "utf8");
+
+get_frequency_total = (dataset) => {
+    const deltas = input.split(/\n/g);
+    deltas.forEach(delta => final_frequency += parseInt(frequency_delta))
+    for (let delta of deltas) {
+        
+    }
+    return final_frequency;
+}
+
+get_first_dupplicate = (input) => {
+    const deltas = input.split(/\n/g);
+    const seen = {};
+    let frequency = 0;
+
+    while (true) {
+        for (const delta of deltas) {
+            frequency += Number(delta);
+            if (seen[frequency]) return frequency;
+            seen[frequency] = true;
+        }
+    }
+};
+
+//let res1 = get_frequency_total(dataset);
+let res2 = get_first_dupplicate(dataset);
+
+console.log("=== Results ===")
+//console.log(`1: ${res1}`);
+console.log(`2: ${res2}`);
